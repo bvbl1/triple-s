11bbab4b39f94c44524914af1dac199b6fbe0908
Abylay <abylaymoldakhmet@gmail.com>
2025-03-05 00:43:10 +0500

    useless commit message

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -18,19 +22,41 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello HTTP!\n")
 }
+
 func main() {
-	var portFlag = flag.String("port", ":8080", "write port for the server")
+	var portFlag = flag.String("port", "8080", "write port for the server")
 	flag.Parse()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(*portFlag, mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	server := &http.Server{
+		Addr:    ":" + *portFlag,
+		Handler: mux,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		fmt.Printf("Server is running on port %s\n", *portFlag)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Ожидание сигнала завершения
+	<-stop
+	fmt.Println("\nShutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error during shutdown: %s\n", err)
+	} else {
+		fmt.Println("Server stopped gracefully")
 	}
 }
